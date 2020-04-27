75e99eaafef2c45fdd21a74fef83ef8ffa93838e
Florian Polier <polier.florian@gmail.com>
2020-04-27 15:10:46 +0200

    code cleaning and data extracted from pcap

--- files/scaircrack.py
+++ files/scaircrack.py
@@ -60,15 +60,11 @@ with open(args.dictionary) as f1 :
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
 ssid        =  wpa[0].info.decode('utf-8')
-#APmac       = a2b_hex("cebcc8fdcab7")
 APmac       = a2b_hex(wpa[0].addr2.replace(':',''))
-#Clientmac   = a2b_hex("0013efd015bd")
 Clientmac   = a2b_hex(wpa[1].addr1.replace(':',''))
 
 # Authenticator and Supplicant Nonces
-#ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
 ANonce      = a2b_hex(wpa[5][EAPOL].load[13:13+0x20].hex())
-#SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 SNonce      = a2b_hex(wpa[6][EAPOL].load[13:13+0x20].hex())
 
 # 7 : ACK with GTK encrypted by KEK and authenticated by KCK
@@ -77,11 +73,10 @@ SNonce      = a2b_hex(wpa[6][EAPOL].load[13:13+0x20].hex())
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-#mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
 mic_to_test = wpa[8][EAPOL].load[-18:-2].hex()
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
+data       = a2b_hex((wpa[8][EAPOL].original[:-20] + b"\x00"*20).hex())
 
 print ("\n\nValues used to derivate keys")
 print ("============================")
--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -45,15 +45,11 @@ wpa=rdpcap("wpa_handshake.cap")
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
 ssid        =  wpa[0].info.decode('utf-8')
-#APmac       = a2b_hex("cebcc8fdcab7")
 APmac       = a2b_hex(wpa[0].addr2.replace(':',''))
-#Clientmac   = a2b_hex("0013efd015bd")
 Clientmac   = a2b_hex(wpa[1].addr1.replace(':',''))
 
 # Authenticator and Supplicant Nonces
-#ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
 ANonce      = a2b_hex(wpa[5][EAPOL].load[13:13+0x20].hex())
-#SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 SNonce      = a2b_hex(wpa[6][EAPOL].load[13:13+0x20].hex())
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
@@ -62,8 +58,7 @@ mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
 
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
-data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
-
+data       = a2b_hex((wpa[8][EAPOL].original[:-20] + b"\x00"*20).hex())
 print ("\n\nValues used to derivate keys")
 print ("============================")
 print ("Passphrase: ",passPhrase,"\n")
@@ -83,7 +78,6 @@ ptk = customPRF512(pmk,str.encode(A),B)
 
 #calculate MIC over EAPOL payload (Michael)- The ptk is, in fact, KCK|KEK|TK|MICK
 mic = hmac.new(ptk[0:16],data,hashlib.sha1)
-
 
 print ("\nResults of the key expansion")
 print ("=============================")
